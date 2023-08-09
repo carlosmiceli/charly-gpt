300d5e0ad7e05710cb4fd34f7790b4b0a225eb6e
carlosmiceli <carlosmiceli87@gmail.com>
2023-08-09 15:53:31 -0300

    finalized analyze doc, chat agent

--- charly-gpt/chatbot/views.py
+++ charly-gpt/chatbot/views.py
@@ -6,6 +6,7 @@ import traceback
 from django.shortcuts import render
 from django.http import JsonResponse
 from langchain.document_loaders import BrowserlessLoader
+from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
@@ -14,6 +15,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.agents import AgentType, Tool, initialize_agent
 from langchain.utilities import SerpAPIWrapper
 from langchain.llms import OpenAI
+from serpapi import GoogleSearch
 
 # Initialize OpenAI and the logger
 openai_api_key = os.environ.get('OPENAI_API_KEY', '')
@@ -31,60 +33,53 @@ def index(request):
     return render(request, 'form.html')
 
 # Analyze Document view
-# Analyze Document view
 def analyze_document(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            input_data = data.get('input', '')
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data.'}, status=400)
 
         try:
             # Load data from the URL
-            logger.info(f"Loading data from URL: {input_data}")
-            response = requests.post(
-                "https://chrome.browserless.io/scrape",
-                params={
-                    "token": os.environ.get('BROWSERLESS_API_KEY', ''),
-                },
-                json={
-                    "url": input_data,
-                    "elements": [{"selector": "body"}],
-                },
-            )
-            # Log the API response data
-            response_data = response.json()
-            logger.info(f"API Response: {response_data}")
+            input_text = data.get('input', '')
+            url = data.get('url', '')
 
-            # Check if the response is a list and extract the page content accordingly
-            if isinstance(response_data, list):
-                if response_data:
-                    if "results" in response_data[0] and response_data[0]["results"]:
-                        page_content = response_data[0]["results"][0].get("text", "")
-                    else:
-                        return JsonResponse({'error': 'No "results" key found in the API response.'}, status=500)
-                else:
-                    return JsonResponse({'error': 'Empty response from the Browserless API.'}, status=500)
-            else:
-                return JsonResponse({'error': 'Invalid response format from the Browserless API.'}, status=500)
+            # Initialize the BrowserlessLoader
+            browserless_loader = BrowserlessLoader(
+                api_token=os.environ.get('BROWSERLESS_API_KEY', ''),
+                urls=[url],  # Load a single URL
+                text_content=True  # Set to False if you want to load raw HTML content
+            )
+
+            # Load Documents from the URL
+            documents = browserless_loader.load()
+
+            # You can now process the loaded Documents as needed
+            page_content = documents[0].page_content
 
             # Split the Document into chunks for embedding and vector storage
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
             all_splits = text_splitter.split_text(page_content)
 
             # Store the splits in the vectorstore
-            from langchain.vectorstores import Chroma
             vectorstore = Chroma.from_texts(texts=all_splits, embedding=OpenAIEmbeddings())
 
-            # Retrieve relevant splits for the given question
+            # Define the query
+            query = input_text
+
+            # Retrieve relevant documents for the query using the retriever (vectorstore)
             retriever = vectorstore.as_retriever()
+
+            # Initialize the RetrievalQA with the retriever
             qa_chain = RetrievalQA.from_chain_type(chat_model, retriever=retriever)
-            result = qa_chain({"query": input_data})
+
+            # Use the retrieved text as context for the query
+            result = qa_chain({"query": query, "context": page_content})
 
             # Get the answer from the result
             if "result" in result and result["result"]:
-                answer = result["result"][0]["text"]
+                answer = result["result"]
                 logger.info(f"Answer from document analysis: {answer}")
             else:
                 answer = "No answer found"
@@ -105,31 +100,46 @@ def chat_agent(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            user_input = data.get('input', '')
-            use_serpapi = data.get('use_serpapi', False)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data.'}, status=400)
 
+        input_text = data.get('input', '')
+        # use_serpapi = data.get('use_serpapi', False)  # Retrieve from payload
+
+        # Always use SerpAPI
+        # use_serpapi = True
+
         # Create a memory for the conversation
         memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
+        
         # Initialize the agent
         tools = []
-        if use_serpapi and search:
-            logger.info("Using SerpAPI for current search tool.")
-            tools.append(
-                Tool(
-                    name="Current Search",
-                    func=search.run,
-                    description="useful for when you need to answer questions about current events or the current state of the world"
-                )
-            )
+        # Commenting out the condition for NOT using SerpAPI
+        # if use_serpapi:
+        logger.info("Using SerpAPI for current search tool.")
+        # Perform the Google search using SerpAPI
+        params = {
+            "engine": "google",
+            "q": input_text,
+            "api_key": "YOUR_SERPAPI_API_KEY_HERE"  # Replace with your SerpAPI API key
+        }
+        search = GoogleSearch(params)
+        logger.info(f"Search: {search}")
+        results = search.get_dict()
+        # Extract the organic results from the search response
+        organic_results = results.get("organic_results", [])
+        # If you want to use the organic_results in your agent_chain, you can append it to the tools list
+        tools.append(Tool(name="Google Search Results", func=lambda x: organic_results, description="Google search results"))
         # Add more tools here if needed
+        logger.info(f"Tools: {tools}")
 
+        # Initialize the agent only with the tools
         agent_chain = initialize_agent(tools, chat_model, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
 
+        logger.info(f"Agent chain: {agent_chain}")
+
         # Run the agent with user input
-        result = agent_chain.run(input=user_input)
+        result = agent_chain.run(input=input_text)
 
         logger.info(f"Result from chat agent: {result}")
 
@@ -138,13 +148,16 @@ def chat_agent(request):
             try:
                 result = json.loads(result)
             except json.JSONDecodeError:
-                return JsonResponse({'error': 'Invalid JSON data in agent response.'}, status=500)
-
-        # Get the final answer from the result
-        final_answer = result.get("action_input", "I'm sorry, there was an issue with the agent's response.")
+                # If the result is not valid JSON, treat it as a successful response
+                final_answer = result
+            else:
+                # If the result is valid JSON, extract the final answer
+                final_answer = result.get("action_input", "I'm sorry, there was an issue with the agent's response.")
+        else:
+            # If the result is already a dictionary, extract the final answer
+            final_answer = result.get("action_input", "I'm sorry, there was an issue with the agent's response.")
 
         # Return the answer as a JSON response
         return JsonResponse({'answer': final_answer})
 
     return JsonResponse({'error': 'Invalid request method.'})
-
